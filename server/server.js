3aeb02ca8667faf2b42baafee349e4ef77f6797d
Samukel Lukhele <kelo.lukhele@gmail.com>
2021-11-24 15:51:03 +0200

    init

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const ApolloServer = require('apollo-server-express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+require('dotenv').config();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT
+
+app.listen(port || 3000, () => console.log(`App is running on port: http://localhost:${port}`));
